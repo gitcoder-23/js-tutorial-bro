4fd70cd9d2a4ada994c59c8e1b1ec12245cadba1
gitcoder-23 <prabhatece21@gmail.com>
2024-05-28 14:43:33 +0530

    new file upload

--- tutorial.js
+++ tutorial.js
@@ -4,40 +4,40 @@
 // PascalCase
 
 // comment
-console.log("higf");
+console.log('higf');
 // variable assign ES5
 
 // Global Scope
 var x = 2;
 var x = 4;
 var y = x;
-console.log("x-value=>", x);
-console.log("y-value=>", y);
+console.log('x-value=>', x);
+console.log('y-value=>', y);
 
 var x = 4;
 //console.log("x-value=>", x);
 
 // ES6 let, const
 let a = 8;
-console.log("a-value=>", a);
+console.log('a-value=>', a);
 
 const z = 8;
-console.log("z-value>>>>>", z);
+console.log('z-value>>>>>', z);
 
 // Syncronous
-console.log("Ram");
-console.log("Mohan");
-console.log("Sayam");
+console.log('Ram');
+console.log('Mohan');
+console.log('Sayam');
 
 // Asyncronous
 
-console.log("India");
+console.log('India');
 
 setTimeout(() => {
-  console.log("America");
+  console.log('America');
 }, 4000);
 
-console.log("Zurich");
+console.log('Zurich');
 
 // Types
 // Premtive= Number, Boolean, float, double, String
@@ -45,50 +45,50 @@ console.log("Zurich");
 
 let m = true; // Boolean
 
-console.log("m-value=>", typeof m);
+console.log('m-value=>', typeof m);
 let n = 3; // Number
 let b = 3.4; // float
 let d = 40; // double
-const str = "Raju"; // String
+const str = 'Raju'; // String
 
 // Object
 const objData = {
   // key: value
-  name: "Subham",
-  class: "v",
+  name: 'Subham',
+  class: 'v',
   isPresent: true,
   marks: 90,
 };
-console.log("objData=>", objData);
+console.log('objData=>', objData);
 
 // Array
 const numArr = [1, 2, 3, 2];
-console.log("numArr=>", numArr);
-console.log("numArr=>", numArr[2]);
-console.log("numArr-2=>", numArr.indexOf(2));
+console.log('numArr=>', numArr);
+console.log('numArr=>', numArr[2]);
+console.log('numArr-2=>', numArr.indexOf(2));
 
-const strArr = ["1", "2", "3", "2", "5"];
-console.log("strArr=>", strArr.length);
+const strArr = ['1', '2', '3', '2', '5'];
+console.log('strArr=>', strArr.length);
 
 const objArr = [
   {
     id: 1,
-    stuName: "Rahul",
-    className: "VII",
+    stuName: 'Rahul',
+    className: 'VII',
     marks: 40,
   },
   {
     id: 2,
-    stuName: "Rahul",
-    className: "VII",
+    stuName: 'Rahul',
+    className: 'VII',
     marks: 40,
   },
   {
     id: 3,
-    stuName: "Rahul",
-    className: "VII",
+    stuName: 'Rahul',
+    className: 'VII',
     marks: 40,
   },
 ];
 
-console.log("objArr=>", objArr, "objArr-length===>", objArr.length);
+console.log('objArr=>pk', objArr, 'objArr-length===>', objArr.length);

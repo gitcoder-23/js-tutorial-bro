2e74e1cac687c3a5940084893308577400d4ba00
gitcoder-23 <prabhatece21@gmail.com>
2024-05-29 16:45:55 +0530

    new array

--- practise_array.js
+++ practise_array.js
@@ -21,14 +21,23 @@ const arrData5 = [8, 10, 20, 22];
 const popData = arrData5.pop();
 console.log('arrData5=>', arrData5);
 
+// Spice deletion operation
 const arrData6 = [1, 2, 10, 4, 5];
 const spliceData = arrData6.splice(0, 3);
 console.log('spliceData=>', spliceData);
 console.log('arrData6=>', arrData6);
 
+// Spice insertion
+
 let rColors = ['red', 'green', 'blue'];
 const rColorsSplice = rColors.splice(3, 0, 'sink', 9);
 console.log('rColors=>', rColors);
+
+// Spice replacement
+
+let lang = ['C', 'C++', 'Java', 'JavaScript', 7];
+const langSplice = lang.splice(0, 2, 'flutter');
+console.log('lang=>', lang);
 
 const arrData7 = [1, 2, 3, 4, 5];
 const sliceData = arrData7.slice(0, 4);

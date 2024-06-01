e9a6a402cbef829ba514a0af655dabf6db881b39
gitcoder-23 <prabhatece21@gmail.com>
2024-06-01 11:47:32 +0530

    new task commit

--- practise_array.js
+++ practise_array.js
@@ -21,19 +21,19 @@ const arrData5 = [8, 10, 20, 22];
 const popData = arrData5.pop();
 console.log('arrData5=>', arrData5);
 
-// Spice deletion operation
+// Splice deletion operation
 const arrData6 = [1, 2, 10, 4, 5];
 const spliceData = arrData6.splice(0, 3);
 console.log('spliceData=>', spliceData);
 console.log('arrData6=>', arrData6);
 
-// Spice insertion
+// Splice insertion
 
 let rColors = ['red', 'green', 'blue'];
 const rColorsSplice = rColors.splice(3, 0, 'sink', 9);
 console.log('rColors=>', rColors);
 
-// Spice replacement
+// Splice replacement
 
 let lang = ['C', 'C++', 'Java', 'JavaScript', 7];
 const langSplice = lang.splice(0, 2, 'flutter');

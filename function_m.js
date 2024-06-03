d6086c8b04f6ce769b452dc3453b02361baef34e
gitcoder-23 <prabhatece21@gmail.com>
2024-06-03 15:24:03 +0530

    🚀

--- function_m.js
+++ function_m.js
@@ -25,3 +25,30 @@ const genFun = (arg) => {
 };
 
 genFun(90);
+
+// logical operator
+// || , && , !
+
+// ? :
+
+// DOM
+
+// BOM
+const alertFun = () => {
+  // alert("Hi");
+  console.log("arg=>", window.location.pathname);
+};
+
+alertFun();
+var timesRun = 0;
+const intervalData = setInterval(() => {
+  // alert("Hi");
+
+  console.log("arg=>");
+  timesRun += 1;
+  if (timesRun === 10) {
+    clearInterval(intervalData);
+  }
+}, 1000);
+
+console.log("intervalData=>", intervalData);
